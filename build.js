c4f214e009181231164cfccae2c85efef6979b2b
Joy Schnittker <dev@joyersch.de>
2024-11-16 12:53:17 +0100

    removed unused packages

--- build.js
+++ build.js
@@ -1,17 +1,28 @@
 // import libs
 const fs = require('fs-extra');
 const path = require('path');
-const marked = require('marked');
 const nunjucks = require('nunjucks');
 
 const paths = {
     output: {
         root: path.join(__dirname, 'public'),
+        css: path.join(__dirname, 'public/css'),
+        js: path.join(__dirname, 'public/js'),
+        fonts: path.join(__dirname, 'public/css/fonts')
     },
     input: {
         statics: path.join(__dirname, 'src/static'),
         views: path.join(__dirname, 'src/views'),
     },
+    bootstrap: {
+        css: path.join(__dirname, 'node_modules/bootstrap/dist/css/bootstrap.min.css'),
+        js: path.join(__dirname, 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'),
+        icon: {
+            css: path.join(__dirname, 'node_modules/bootstrap-icons/font/bootstrap-icons.css'),
+            fonts: path.join(__dirname, 'node_modules/bootstrap-icons/font/fonts'),
+        },
+
+    }
 };
 
 nunjucks.configure(paths.input.views, {
@@ -37,11 +48,17 @@ const renderAsHtml = (content, title) => `
 </html>
 `;
 
-if (!fs.pathExistsSync(paths.output.root)) {
-    fs.mkdirSync(paths.output.root);
-}
-
+fs.ensureDirSync(paths.output.root);
+fs.ensureDirSync(paths.output.css);
+fs.ensureDirSync(paths.output.js);
+fs.ensureDirSync(paths.output.fonts);
 fs.ensureDirSync(paths.input.statics);
+fs.ensureDirSync(paths.input.views);
+
+fs.copyFileSync(paths.bootstrap.css, path.join(paths.output.css, 'bootstrap.css'));
+fs.copyFileSync(paths.bootstrap.js, path.join(paths.output.js, 'bootstrap.js'));
+fs.copyFileSync(paths.bootstrap.icon.css, path.join(paths.output.css, 'bootstrap-icons.css'));
+fs.copySync(paths.bootstrap.icon.fonts, paths.output.fonts);
 
 const statics = fs.readdirSync(paths.input.statics);
 
--- src/index.js
+++
@@ -1,3 +0,0 @@
-// load all js to be run on the website
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
