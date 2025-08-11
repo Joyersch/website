db81d0962e455dc78a8965e5d926addbfa8458f4
Joy Schnittker <dev@joyersch.de>
2025-08-11 17:49:39 +0200

    Updated about pages

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,11 +1,37 @@
 <div class="ml-3 mr-3">
-    <div class="mt-3">
-        You would not believe how much information some people are willing to
-        give away for free.
+    <p class="text-xl">Me</p>
+    <div class="mt-3 mb-8">
+        <p>Hi, my name is joy and i do coding stuff.</p>
+        <p>
+            I also love making games even though finishing a project is very
+            hard :C.
+        </p>
+        <p class="mt-6 mb-3">
+            On the projects page, you can find all current projects of mine and
+            on the archive page, you can find all my abandoned projects which I
+            do not maintain anymore.
+        </p>
+
+        <div class="mt-8">
+            <div class="mb-5">
+                <p>You can also find me on:</p>
+            </div>
+            <a
+                class="hover:text-neya_second"
+                href="https://github.com/Joyersch"
+            >
+                github
+            </a>
+            <a class="hover:text-neya_second" href="https://joyersch.itch.io/">
+                itch.io
+            </a>
+            <a
+                class="hover:text-neya_second"
+                href="https://www.linkedin.com/in/joy-7ejnma/"
+            >
+                linkedin
+            </a>
+        </div>
+        <div class="mt-2"></div>
     </div>
-    <div class="mt-3">
-        With that beeing said, mine are out there...
-    </div>
-    <div class="mt-3">Happy hunting :3</div>
-    <div class="mt-10">PS: Check the projects page</div>
 </div>
